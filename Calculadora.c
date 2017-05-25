5c90a6284683f72fe7494457282d328ea9adb7eb
Eduardo Carvalho <a16027@bcc.unifal-mg.edu.br>
2017-05-25 19:27:07 -0300

    Adição da função subtrai

--- Calculadora.c
+++ Calculadora.c
@@ -4,12 +4,15 @@
 soma(int x, int y){
  return(x+y);
 }
-subtrai(){}
+subtrai(int x , int y){
+ return(x-y);
+}
 multiplica(){}
 divide(){}
 
 void main(){
 	printf("Hello world\n");
 	printf("%d\n",soma(2,2));
+	printf("%d\n",subtrai(2,2));
 }
 	
