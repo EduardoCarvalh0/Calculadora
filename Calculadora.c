4365806ce4b0e7e67bcf5ee2ea347cd585966857
Eduardo Carvalho <a16027@bcc.unifal-mg.edu.br>
2017-05-25 19:43:39 -0300

    Adição da funcao divisao

--- Calculadora.c
+++ Calculadora.c
@@ -10,12 +10,15 @@ subtrai(int x , int y){
 multiplica(int x, int y){
 	return(x*y);
 }
-divide(){}
+divide(int x, int y){
+	return(x/y);
+}
 
 void main(){
 	printf("Hello world\n");
 	printf("%d\n",soma(2,2));
 	printf("%d\n",subtrai(2,2));
 	printf("%d\n",multiplica(2,3));
+	printf("%d\n",divide(6/2));
 }
 	
